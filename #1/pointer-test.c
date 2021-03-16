6ad9a856ede383a07775603649c587c04cc4d2aa
DohyunJegal <80775466+DohyunJegal@users.noreply.github.com>
2021-03-17 01:09:42 +0900

    Create pointer-test.c

---
+++ #1/pointer-test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void main(){
+    int i, *p;
+    i = 10;
+
+    printf("value of i = %d\n", i);      
+    printf("address of i = %p\n", &i);  
+    printf("value of p = %p\n", p);     
+    printf("address of p = %p\n", &p); 
+
+    p = &i;                                
+
+    printf("\n\n----- after p = &i ------------\n\n");
+
+    printf("value of p = %p\n", p);        
+    printf("address of p = %p\n", &p);     
+    printf("dereferencing *p = %d\n", *p);  
+}
